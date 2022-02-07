562a384162ae96db9f13a2d1441a2bb46110128f
angyRincon <grincon@celerik.com>
2022-02-07 16:25:02 -0500

    Added funtionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const route = '/assets/alpaca'
+    const [accessory, setAccessory] = useState('')
+    const [index, setIndex] = useState(0)
+    const [styleArray, setStyleArray] = useState(data.initialData)
+
+    const handleChangeAccessory = (localAccessory, i) => {
+        setIndex(i)
+        setAccessory(localAccessory)
+    }
+
+    const handleChangeStyle = (localStyle) => {
+        const alpacaChanged = styleArray.map(alpaca => {
+            if (alpaca.accessory === accessory) alpaca.style = localStyle
+            return alpaca
+        })
+        setStyleArray(alpacaChanged)
+    }
+
+    const handleRandom = () => {
+        let arrayStyles = []
+
+        for (let styleValue of styleArray) {
+            for (let value of data.buttonStyles) {
+                const randomIndex = Math.floor(Math.random() * value[Object.keys(value)[0]].length)
+                const newObject = {
+                    accessory: Object.keys(value)[0],
+                    style: value[Object.keys(value)[0]][randomIndex]
+                }
+                if (styleValue.accessory === newObject.accessory && styleValue.accessory) {
+                    styleValue.style = newObject.style
+                    arrayStyles = [...arrayStyles, {accessory: styleValue.accessory, style: styleValue.style}]
+                }
+            }
+        }
+        setStyleArray(arrayStyles)
+    }
+
+    return (
+        <div className='container'>
+            <div className="grid-container">
+                <div className='alpaca-images'>
+                    <div className="images">
+                        {
+                            styleArray.map(img => (
+                                <img
+                                    key={img.accessory}
+                                    src={`${route}/${img.accessory}/${img.style}.png`}
+                                    alt={`${img.accessory}-${img.style}`}
+                                />
+                            ))
+                        }
+                    </div>
+                    <div className='buttons'>
+                        <button onClick={handleRandom}>Random</button>
+                        <button>Download</button>
+                    </div>
+                </div>
+
+                <div className='buttonsContainer'>
+                    <h2>Accessorize the alpaca</h2>
+                    <div className='button-accessories-container'>
+                        {
+                            data.buttonsAccessories.map((button, i) => (
+                                <button
+                                    key={button}
+                                    className='button-accessories'
+                                    onClick={() => handleChangeAccessory(button, i)}
+                                >
+                                    {button}
+                                </button>
+                            ))
+                        }
+                    </div>
+
+                    <h2>Style</h2>
+                    <div>
+                        {
+                            data.buttonStyles[index][accessory || 'hair'].map(style => (
+                                    <button
+                                        key={style}
+                                        className='button-accessories'
+                                        onClick={() => handleChangeStyle(style)}
+                                    >
+                                        {style}
+                                    </button>
+                                )
+                            )
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/data/index.js
@@ -0,0 +1,10 @@
+import buttonsAccessories from './buttonsAccessories.json'
+import buttonStyles from './buttonStyles.json'
+import initialData from './initialData.json'
+
+const data = {
+    buttonsAccessories,
+    buttonStyles,
+    initialData
+}
+export default data
